3cc25df81d6eff9e826772fbdcef1569264518a4
mdkulikowski <mdkulikowski@gmail.com>
2024-08-30 21:25:45 -0500

    added some of the controller logic

--- Keyboard_To_Generic_Controller.ino
+++ Keyboard_To_Generic_Controller.ino
@@ -4,34 +4,56 @@ USBHost myusb;
 KeyboardController keyboard1(myusb);
 USBHIDParser hid1(myusb);
 
+
 void OnPress(int key){
-  digitalWrite(LED_BUILTIN, HIGH);
   switch (key){
-    case 'w': Serial.print('w'); break;
-    case 'a': Serial.print('a'); break;
-    case 's': Serial.print('s'); break;
-    case 'd': Serial.print('d'); break;
-    case ' ': Serial.print("space"); break;
-    case 'j': Serial.print('j'); break;
-    case 'k': Serial.print('k'); break;
-    case 'l': Serial.print('l'); break;
-    case ';': Serial.print(';'); break;
-    case 'u': Serial.print('u'); break;
-    case 'i': Serial.print('i'); break;
-    case 'o': Serial.print('o'); break;
-    case 'n': Serial.print('n'); break;
-    case 'm': Serial.print('m'); break;
-    case ',': Serial.print(','); break;
+    case 'w': Joystick.Y(1023); break;
+    case 'a': Joystick.X(0); break;
+    case 's': Joystick.Y(0); break;
+    case 'd': Joystick.x(1023); break;
+    case ' ': Joystick.button(4,1); break;
+    case 'j': Joystick.button(1,1); break;
+    case 'k': Joystick.button(2,1); break;
+    case 'l': Joystick.button(3,1); break;
+    case ';': Joystick.button(11,1); break;
+    case 'u': Joystick.button(5,1); break;
+    case 'i': Joystick.button(6,1); break;
+    case 'ESC': Joystick.button(10,1); break;
+    case 'CR': Joystick.button(9,1); break;
+    case 'o': Joystick.button(12,1); break;
+    case 'n': Joystick.button(13,1); break;
+    case 'm': Joystick.button(14,1); break;
+    case ',': Joystick.button(15,1); break;
   }
-  Serial.print(" ");
 }
 
+//void onJoystickPress()
+
 void OnRelease(int key){
-  digitalWrite(LED_BUILTIN, LOW);
+  switch (key){
+    case 'w': Joystick.Y(512); break;
+    case 'a': Joystick.X(512); break;
+    case 's': Joystick.Y(512); break;
+    case 'd': Joystick.x(512); break;
+    case ' ': Joystick.button(4,0); break;
+    case 'j': Joystick.button(1,0); break;
+    case 'k': Joystick.button(2,0); break;
+    case 'l': Joystick.button(3,0); break;
+    case ';': Joystick.button(11,0); break;
+    case 'u': Joystick.button(5,0; break;
+    case 'i': Joystick.button(6,0); break;
+    case 'ESC': Joystick.button(10,0); break;
+    case 'CR': Joystick.button(9,0); break;
+    case 'o': Joystick.button(12,0); break;
+    case 'n': Joystick.button(13,0); break;
+    case 'm': Joystick.button(14,0); break;
+    case ',': Joystick.button(15,0); break;
+  }
 }
 
 void setup() {
   myusb.begin();
+  Joystick.begin();
   keyboard1.attachPress(OnPress);
   keyboard1.attachRelease(OnRelease);
   pinMode(LED_BUILTIN, OUTPUT);
