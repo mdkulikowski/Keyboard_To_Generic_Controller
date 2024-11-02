18c86e5e1647d5e3df167074f8fa3a92c0c81c48
mdkulikowski <mdkulikowski@gmail.com>
2024-11-02 02:43:40 -0500

    fixed R3, enabled caps lock, moved select to e - only issue is that capital space can be a lot of different keys as well...

--- Keyboard_To_Generic_Controller/Keyboard_To_Generic_Controller.ino
+++ Keyboard_To_Generic_Controller/Keyboard_To_Generic_Controller.ino
@@ -8,46 +8,75 @@ const uint8_t escape_key = 27;
 const uint8_t enter_key = 10;
 
 void OnPress(int key){
+  Serial.println(key);
   switch (key){
+    case 'W':
     case 'w': XInput.press(DPAD_UP); break;
+    case 'A':
     case 'a': XInput.press(DPAD_LEFT); break;
+    case 'S':
     case 's': XInput.press(DPAD_DOWN); break;
+    case 'D':
     case 'd': XInput.press(DPAD_RIGHT); break;
+    case 0:
     case ' ': XInput.press(JOY_LEFT); break;
+    case 'J':
     case 'j': XInput.press(BUTTON_A); break;
+    case 'K':
     case 'k': XInput.press(BUTTON_B); break;
+    case 'L':
     case 'l': XInput.press(BUTTON_X); break;
     case ';': XInput.press(BUTTON_Y); break;
+    case 'U':
     case 'u': XInput.press(TRIGGER_LEFT); break;
+    case 'I':
     case 'i': XInput.press(TRIGGER_RIGHT); break;
     case escape_key: XInput.press(BUTTON_START); break;
-    case enter_key: XInput.press(BUTTON_BACK); break;
-    case 'o': XInput.press(TRIGGER_RIGHT); break;
+    case 'E':
+    case 'e': XInput.press(BUTTON_BACK); break;
+    case 'O':
+    case 'o': XInput.press(JOY_RIGHT); break;
+    case 'N':
     case 'n': XInput.press(BUTTON_LB); break;
+    case 'M':
     case 'm': XInput.press(BUTTON_RB); break;
-    case ',': XInput.press(BUTTON_LOGO); break;
+    case enter_key: XInput.press(BUTTON_LOGO); break;
   }
 }
 
 void OnRelease(int key){
   switch (key){
+    case 'W':
     case 'w': XInput.release(DPAD_UP); break;
+    case 'A':
     case 'a': XInput.release(DPAD_LEFT); break;
+    case 'S':
     case 's': XInput.release(DPAD_DOWN); break;
+    case 'D':
     case 'd': XInput.release(DPAD_RIGHT); break;
+    case 0:
     case ' ': XInput.release(JOY_LEFT); break;
+    case 'J':
     case 'j': XInput.release(BUTTON_A); break;
+    case 'K':
     case 'k': XInput.release(BUTTON_B); break;
+    case 'L':
     case 'l': XInput.release(BUTTON_X); break;
     case ';': XInput.release(BUTTON_Y); break;
+    case 'U':
     case 'u': XInput.release(TRIGGER_LEFT); break;
+    case 'I':
     case 'i': XInput.release(TRIGGER_RIGHT); break;
     case escape_key: XInput.release(BUTTON_START); break;
-    case enter_key: XInput.release(BUTTON_BACK); break;
-    case 'o': XInput.release(TRIGGER_RIGHT); break;
+    case 'E':
+    case 'e': XInput.release(BUTTON_BACK); break;
+    case 'O':
+    case 'o': XInput.release(JOY_RIGHT); break;
+    case 'N':
     case 'n': XInput.release(BUTTON_LB); break;
+    case 'M':
     case 'm': XInput.release(BUTTON_RB); break;
-    case ',': XInput.release(BUTTON_LOGO); break;
+    case enter_key: XInput.release(BUTTON_LOGO); break;
   }
 }
 
@@ -55,6 +84,7 @@ void setup() {
   myusb.begin();
   keyboard1.attachPress(OnPress);
   keyboard1.attachRelease(OnRelease);
+  Serial.begin(9600);
 }
 
 void loop() {
